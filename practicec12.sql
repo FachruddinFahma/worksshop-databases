04e07151997c7f4518ade6f7b6d0f46d91631722
adzazarif <adzazarf@gmail.com>
2022-11-22 11:24:19 +0700

    push kedua

--- practicec12.sql
+++ practicec12.sql
@@ -15,3 +15,20 @@ SELECT mhs.nim, mhs.nama_mhs, mhs.alamat,
     -> WHERE mhs.kode_jur = jurusan.kode_jur
     -> AND mhs.nip = dosen.nip
     -> ORDER BY jurusan.nama_jur DESC;
+
+-- Menampilkan nama jurusan, nama dosen, jumlah mahasiswa dari setiap dosen
+-- pada masing-masing jurusan
+SELECT jurusan.nama_jur, dosen.nama_dosen,
+    -> COUNT(mhs.nim) AS jml_mhs
+    -> FROM jurusan, mhs, dosen
+    -> WHERE jurusan.kode_jur = mhs.kode_jur
+    -> AND dosen.nip = mhs.nip
+    -> GROUP BY jurusan.nama_jur, dosen.nama_dosen;
+
+-- Menampilkan nama dosen, nama jurusan, jumlah mata kuliah dari masingmasing dosen pada setiap jurusan.
+SELECT dosen.nama_dosen, jurusan.nama_jur,
+    -> COUNT(mata_kuliah.kode_mk) AS jumlah_mk
+    -> FROM mata_kuliah, dosen, jurusan
+    -> WHERE mata_kuliah.nip = dosen.nip
+    -> AND mata_kuliah.kode_jur = jurusan.kode_jur
+    -> GROUP BY dosen.nama_dosen, jurusan.nama_jur;
