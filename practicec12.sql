81ec15d114452e8493e420349f7379491ae32ade
adzazarif <adzazarf@gmail.com>
2022-11-23 15:46:33 +0700

    tambah soal

--- practicec12.sql
+++ practicec12.sql
@@ -25,7 +25,8 @@ SELECT jurusan.nama_jur, dosen.nama_dosen,
     -> AND dosen.nip = mhs.nip
     -> GROUP BY jurusan.nama_jur, dosen.nama_dosen;
 
--- Menampilkan nama dosen, nama jurusan, jumlah mata kuliah dari masingmasing dosen pada setiap jurusan.
+-- Menampilkan nama dosen, nama jurusan, jumlah mata kuliah dari masingmasing 
+-- dosen pada setiap jurusan.
 SELECT dosen.nama_dosen, jurusan.nama_jur,
     -> COUNT(mata_kuliah.kode_mk) AS jumlah_mk
     -> FROM mata_kuliah, dosen, jurusan
@@ -53,3 +54,23 @@ SELECT dosen.nama_dosen, jurusan.nama_jur,
     -> GROUP BY dosen.nama_dosen, jurusan.nama_jur
     -> HAVING jml_sks BETWEEN 5 AND 12
     -> ORDER BY jml_sks DESC;
+
+-- Menampilkan kode krs, tanggal krs, nama jurusan, nama mahasiswa,
+-- jumlah sks dan diurutkan berdasarkan jumlah terbesar.
+SELECT krs.kode_krs, krs.tanggal_krs,
+    -> mhs.nama_mhs, krs.jumlah_sks
+    -> FROM krs JOIN jurusan
+    -> ON krs.kode_jur = jurusan.kode_jur
+    -> JOIN mhs
+    -> ON krs.nim = mhs.nim
+    -> ORDER BY krs.jumlah_sks DESC;
+
+-- Menampilkan kode krs, tanggal krs, hari, jam, nama matakuliah dan
+-- diurutkan berdasarkan hari yang paling awal.
+SELECT krs.kode_krs, krs.tanggal_krs,
+    -> detail_krs.hari, detail_krs.jam, mata_kuliah.nama_mk
+    -> FROM krs JOIN detail_krs
+    -> ON krs.kode_krs = detail_krs.kode_krs
+    -> JOIN mata_kuliah
+    -> ON krs.kode_jur = mata_kuliah.kode_jur
+    -> ORDER BY detail_krs.hari ASC;
